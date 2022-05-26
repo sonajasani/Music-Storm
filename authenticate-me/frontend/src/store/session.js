3ccf1aedcd8a3c7a8f821e1887eb9cd29bedd0dc
Sona Jasani <jasanisona@gmail.com>
2022-05-26 19:16:28 -0400

    logout thunk action

--- authenticate-me/frontend/src/store/session.js
+++ authenticate-me/frontend/src/store/session.js
@@ -63,6 +63,14 @@ export const signup = (user) => async (dispatch) => {
 };
 
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 /***************************************************************************************************/ 
 
 const initialState = { user: null };
