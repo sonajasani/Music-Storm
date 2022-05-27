c6f1a9365c5cc7db88aefc1c84230ba280f44b84
Sona Jasani <jasanisona@gmail.com>
2022-05-27 01:25:15 -0400

    heroku settings

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -6,7 +6,7 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-
+/***********************************************************************************************************************/
 
 // Static routes
 // Serve React build files in production
@@ -37,16 +37,10 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
       res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.json({});
+      return res.status(201).json({});
     });
 }
 
-
-
-  
-
-
-
-
+/********************************************************************************************************************/
 
 module.exports = router;
