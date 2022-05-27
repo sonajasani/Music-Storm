13a9a951de34e54fee517ae670e2f49ba8afb5e7
Sona Jasani <jasanisona@gmail.com>
2022-05-27 16:59:04 -0400

    songs actions

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 
@@ -10,6 +11,17 @@ import './Navigation.css';
 
 
 function Navigation({ isLoaded }){
+
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCredential("Demo-lition")
+    setPassword("password")
+    return dispatch(sessionActions.login({ credential, password }))
+}
 
   const sessionUser = useSelector(state => state.session.user);
 
@@ -23,6 +35,17 @@ function Navigation({ isLoaded }){
       <>
         <NavLink to="/login">Log In</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
+        <form onSubmit={handleSubmit}>
+        <input
+          type="hidden"
+          value={credential}
+        />
+        <input
+          type="hidden"
+          value={password}
+        />
+        <button type="submit">Demo</button>
+        </form>
       </>
     );
   }
