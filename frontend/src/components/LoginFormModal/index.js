13918e3ef0345737fc6fb0d1428d54502e14cb8c
Sona Jasani <jasanisona@gmail.com>
2022-05-30 23:28:48 -0400

    login and signup modal

---
+++ frontend/src/components/LoginFormModal/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Modal from "react-modal";
+import { useSelector, useDispatch } from "react-redux";
+
+import { openLogin, closeLogin } from "../../store/modal";
+import LoginForm from "./LoginForm";
+import "./LoginForm.css";
+
+Modal.setAppElement(document.getElementById("root"));
+
+/************************************************************************************************************/
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+function LoginFormModal() {
+  
+  const dispatch = useDispatch();
+
+  const loginState = useSelector((state) => state.modal.loginShow);
+
+  const closeModal = () => dispatch(closeLogin());
+
+  return (
+    <>
+      <button
+        id="signInBtn"
+        className="nav-bar-btn"
+        onClick={() => dispatch(openLogin())}
+      >
+        Sign In
+      </button>
+      <Modal
+        isOpen={loginState}
+        closeTimeoutMS={500}
+        onRequestClose={closeModal}
+        // style={customStyles}
+        contentLabel="Login Modal"
+        overlayClassName="OuterModal"
+        className="InnerModal"
+      >
+        <LoginForm />
+      </Modal>
+    </>
+  );
+}
+
+/************************************************************************************************************/
+
+export default LoginFormModal;
