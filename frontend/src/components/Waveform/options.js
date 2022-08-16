73976485efe371ae4383d4cea24fd1abdc2cbf7d
Sona Jasani <jasanisona@gmail.com>
2022-08-16 12:03:18 -0400

    waveform component added

---
+++ frontend/src/components/Waveform/options.js
@@ -0,0 +1,11 @@
+export const waveformOptions = {
+    backend: 'MediaElement',
+    progressColor: '#f50',
+    responsive: true,
+    interact: false,
+    normalize: true,
+    barGap: 1.1,
+    barWidth: 2,
+    cursorColor: 'transparent',
+    barHeight: 1,
+  };
